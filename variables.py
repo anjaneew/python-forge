0ff9ce1872de4adacd7643ddf1368833687c9510
Anjanee Wijewardana <anjaneew@gmail.com>
2026-01-07 11:10:51 +0000

    variables

---
+++ variables.py
@@ -0,0 +1,21 @@
+#Variables
+
+#data-types - string, integer, float, boolean
+
+myhouse = "Malwana"
+family = 5
+price = 9800.85
+isResident = False
+
+#f-strings
+
+print(f"I am from {myhouse}. It is now worth ${price}. We have {family} members. But is it my current residence? {isResident}")
+
+isStudent = True
+
+#If statements:
+
+if isStudent:
+    print("I am a student.")
+else:
+    print("I am not a student.")    
