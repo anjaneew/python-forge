69cb72b4efcac9f80a55b23574453c188f4b4467
Anjanee Wijewardana <anjaneew@gmail.com>
2026-01-07 17:46:07 +0000

    If statements

---
+++ if_statements.py
@@ -0,0 +1,10 @@
+response = input("Are you hungry? (Y/N): ")
+
+if response == "Y" :
+    print("Here is some food.")
+
+elif response == "N":
+    print("Then you can have some refreshing drink.")
+
+else: 
+    print("We cannot read your response. ")    
