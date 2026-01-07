4e4c4c8368c36d0f21e6379c38d03260cb6f064c
Anjanee Wijewardana <anjaneew@gmail.com>
2026-01-07 16:08:24 +0000

    typecasting

---
+++ typecasting.py
@@ -0,0 +1,15 @@
+#Typecasting 
+#            str(), int(), float(), bool()
+
+id1 = "12538"
+id2 = "12584"
+
+print(id1 + id2) #1253812584
+
+print(type(id1)) #<class 'str'>
+print(type(id2)) #<class 'str'>
+
+id1 = int("12538")
+id2 = int("12584")
+
+print(id1 + id2) #25122
