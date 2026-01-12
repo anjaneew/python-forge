ff651ea6736347a93911253bba054853e86083a5
Anjanee Wijewardana <anjaneew@gmail.com>
2026-01-12 17:27:55 +0000

    nested loop

---
+++ nested_loop.py
@@ -0,0 +1,12 @@
+import time
+
+# nested loop
+
+# outer loop:
+#   inner loop:
+
+#my version
+for x in range(1,11):
+    for y in range(1,4):
+        time.sleep(3)
+        print(f"person {x} is getting package {y}.")
