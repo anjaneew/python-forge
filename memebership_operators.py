605943cdf442a11f334cbecbc4cd2bf27322a37d
Anjanee Wijewardana <anjaneew@gmail.com>
2026-01-15 14:59:18 +0000

    membership operators

---
+++ memebership_operators.py
@@ -0,0 +1,25 @@
+# Membership operators = test if a value/varable is found in 
+#                           a sequence (string, list, tuple, set,   
+#                           dictionary) - in   - not in
+
+#Example 1
+word = "apple"
+
+letter = input("Guess a letter: ")
+
+if letter not in word: # case sensitive
+    print(f"{letter} is not found in the secret word.")
+else:
+    print(f"{letter} is found in the secret word.")
+
+
+#Example 2
+
+suspects = { "Robin", "Marion" , "March"}
+
+suspect = input("Enter the suspect: ")
+
+if suspect in suspects:
+    print(f"{suspect} is the criminal.")
+else:
+    print(f"{suspect} is innocent.")   
