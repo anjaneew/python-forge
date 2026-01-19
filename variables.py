c079aa9765a471001fa2b694a25c3333813f2e02
Anjanee Wijewardana <anjaneew@gmail.com>
2026-01-19 08:06:32 +0000

    variables

--- variables.py
+++ variables.py
@@ -19,3 +19,19 @@ if isStudent:
     print("I am a student.")
 else:
     print("I am not a student.")    
+
+#multiple variables 
+ 
+#  'Hello World', to 3 variables 
+
+x = y = z = "Hello World"
+print(x) #Hello World
+print(y) #Hello World
+print(z) #Hello World
+
+# values to multiple variables in one line
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x) # Orange
+print(y) # Banana
+print(z) # Cherry
