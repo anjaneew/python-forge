b6d48778acabb51ae58fefd21c768ac2f5d7dd47
Anjanee Wijewardana <anjaneew@gmail.com>
2026-01-19 07:30:41 +0000

    modules

---
+++ about_module.py
@@ -0,0 +1,21 @@
+# print(help("modules"))
+
+# print(help("time"))
+# print(help("turtle"))
+
+import string
+#improting as alias
+import time as my_time_machine
+#from
+from math import e
+
+# print(math.pi) #3.141592653589793
+print(e) # 2.718281828459045
+
+
+# print(help("string"))
+print(string.digits) # 0123456789
+
+a, b, c, d = 1, 2, 3, 4
+
+print(e ** c) # 20.085536923187664
