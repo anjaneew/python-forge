925fc6ae7db1fd9b1e20850e18ce0e097d827ca8
Anjanee Wijewardana <anjaneew@gmail.com>
2026-01-20 18:01:46 +0000

    hangman game

---
+++ projects/hangman_game.py
@@ -0,0 +1,95 @@
+# Hangman in python
+import random
+from word_list import words 
+
+# #local variable
+# words = ("kite", "war", "sea", "amma")
+
+# ASCII ART dictionary of key:() 
+hangman_art = {0: ("   ",
+                   "   ",
+                   "   "), 
+               1: (" o ",
+                   "   ",
+                   "   "),
+               2: (" o ",
+                   " | ",
+                   "   "),
+               3: (" o ",
+                   "/| ",
+                   "   "),
+               4: (" o ",
+                   "/|\\",
+                   "   "),
+               5: (" o ",
+                   "/|\\",
+                   "/  "),
+               6: (" o ",
+                   "/|\\",
+                   "/ \\"),}
+
+# #display art
+# for line in hangman_art[3]:
+#     print(line)
+
+def display_man(wrong_guesses):
+    print("***************")
+    for line in hangman_art[wrong_guesses]:
+        print(line)
+    print("***************")
+
+def display_hint(hint):
+    print(" ".join(hint))
+
+def display_answer(answer):
+    print(" ".join(answer))
+
+def main():
+    answer = random.choice(words)
+    # print(answer)
+    hint = ["_"] * len(answer)
+    # print(hint)
+    wrong_guesses = 0
+    guessed_letters = set()
+    is_running = True
+
+    while is_running:
+        display_man(wrong_guesses)
+        display_hint(hint)
+        guess = input("Enter a letter: ").lower()
+
+        #input validation
+        if len(guess) != 1 or not guess.isalpha():
+            print("invalid input!")
+            continue
+        
+        if guess in guessed_letters:
+            print(f"{guess} is already guessed.")
+            continue
+
+        guessed_letters.add(guess)
+
+        if guess in answer:
+            for i in range(len(answer)):
+                if answer[i] == guess:
+                    hint[i] = guess
+        else:
+            wrong_guesses += 1
+
+        if "_" not in hint:
+            display_man(wrong_guesses)
+            display_answer(answer)
+            print("***************")
+            print("You win!")
+            print("***************")
+            is_running = False
+        elif wrong_guesses >= len(hangman_art) - 1:
+            display_man(wrong_guesses)
+            display_answer(answer)
+            print("***************")
+            print("You Lose!")
+            print("***************")
+            is_running = False
+
+if __name__ == '__main__':
+    main()
---
+++ projects/word_list.py
@@ -0,0 +1,10 @@
+# word list for a hangman game
+
+words = (
+    "cat", "dog", "sun", "moon", "tree", "book", "ball", "fish", "bird", "star",
+    "milk", "rain", "wind", "fire", "leaf", "road", "home", "cake", "hand", "foot",
+    "ship", "farm", "frog", "duck", "snow", "rock", "hill", "ring", "lamp", "door",
+    "apple", "banana", "grape", "bread", "chair", "table", "pencil", "paper", "clock",
+    "shirt", "shoe", "grass", "water", "river", "cloud", "smile", "laugh", "sleep",
+    "amma", "kite", "war", "sea"
+)
