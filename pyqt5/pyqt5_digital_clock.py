0e4a87c55fed09ea1ab2019ea412719270b9b8fa
Anjanee Wijewardana <anjaneew@gmail.com>
2026-01-31 11:12:26 +0000

    Python PyQt5 Digital Clock

--- pyqt5/pyqt5_digital_clock.py
+++ pyqt5/pyqt5_digital_clock.py
@@ -1,13 +1,16 @@
 # Python PyQt5 Digital Clock
 
+# Fonts - Ds Digital - TTF file (.ttf TrueType Font)
+
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
 from PyQt5.QtCore import QTimer, QTime, Qt
+from PyQt5.QtGui import QFont, QFontDatabase
 
 class DigitalClock(QWidget):
     def __init__(self):
         super().__init__()
-        self.time_label = QLabel("12:00:00", self)
+        self.time_label = QLabel(self)
         self.timer = QTimer(self)
         self.initUI()
 
@@ -19,8 +22,27 @@ class DigitalClock(QWidget):
         vbox.addWidget(self.time_label)   
         self.setLayout(vbox)
 
+        # styling
         self.time_label.setAlignment(Qt.AlignCenter)
-        self.time_label.setStyleSheet("font-size: 150px;") # 10:40
+        self.time_label.setStyleSheet("font-size: 100px;" 
+                                      "color: #34f52a") 
+        self.setStyleSheet("background-color: #181f18")
+
+        # custom font
+        font_id = QFontDatabase.addApplicationFont("pyqt5/DS-DIGIT.TTF")
+        font_family = QFontDatabase.applicationFontFamilies(font_id)[0]
+        my_font = QFont(font_family, 150)
+        self.time_label.setFont(my_font)
+
+        # add functionality
+        self.timer.timeout.connect(self.update_time) # update regularly
+        self.timer.start(1000) # update every 1 second
+
+        self.update_time() 
+
+    def update_time(self): 
+        current_time = QTime.currentTime().toString("hh:mm:ss AP") # AP - AM PM
+        self.time_label.setText(current_time) 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
