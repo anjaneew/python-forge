ef23f4cf97d7dc87e9213f791bc95f1eac184ecc
Anjanee Wijewardana <anjaneew@gmail.com>
2026-02-04 14:28:16 +0000

    Expenses Tracker

--- my_projects/expenses_tracker.py
+++ my_projects/expenses_tracker.py
@@ -14,14 +14,16 @@ class ExpensesApp(QWidget):
         self.amount_input = QDoubleSpinBox(self)
         self.date_label = QLabel("Date: ", self)
         self.data_input = QDateEdit(self)
-        self.submit_button = QPushButton("Submit", self)
-        self.details_label = QLabel(self)
+        self.submit_button = QPushButton("Submit new details", self)
+        self.details_label = QLabel("Display Expenses List", self)
+        self.display_button = QPushButton("Generate List", self)
 
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle("~Expenses Tracker~")
 
+        # layout
         vbox = QVBoxLayout()
 
         vbox.addWidget(self.title_label)
@@ -33,14 +35,19 @@ class ExpensesApp(QWidget):
         vbox.addWidget(self.data_input)
         vbox.addWidget(self.submit_button)
         vbox.addWidget(self.details_label)
+        vbox.addWidget(self.display_button)
 
         self.setLayout(vbox)
 
-    def get_expenses():
-        pass
+        # Category - add items, connect signals
+        self.category_input.addItems(["Groceries", "Transport", "Communication", "Rent", "Medicine"])
+        self.category_input.currentTextChanged.connect(self.get_expenses)
 
-    def store_expenses():
-        pass
+    def get_expenses(self, category):
+        return category
+
+    def store_expenses(self):
+        self.get_expenses()
 
     def display_expenses():
         pass
