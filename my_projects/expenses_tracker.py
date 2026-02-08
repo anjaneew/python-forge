fac304005d6cd20c44fe6d8a23dac6f89f3e92f5
Anjanee Wijewardana <anjaneew@gmail.com>
2026-02-08 17:30:59 +0000

    expenses tracker

--- my_projects/expenses_tracker.py
+++ my_projects/expenses_tracker.py
@@ -7,6 +7,7 @@ from PyQt5.QtCore import Qt
 class ExpensesApp(QWidget):
     def __init__(self):
         super().__init__()
+        # creating widgets
         self.title_label = QLabel("Enter your expenses", self)
         self.category_label = QLabel("Category: ", self)
         self.category_input = QComboBox(self)
@@ -15,15 +16,14 @@ class ExpensesApp(QWidget):
         self.date_label = QLabel("Date: ", self)
         self.data_input = QDateEdit(self)
         self.submit_button = QPushButton("Submit new details", self)
-        self.details_label = QLabel("Display Expenses List", self)
-        self.display_button = QPushButton("Generate List", self)
+        self.display_button = QPushButton("Display Expenses List", self)
 
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle("~Expenses Tracker~")
 
-        # layout
+        # verticle layout
         vbox = QVBoxLayout()
 
         vbox.addWidget(self.title_label)
@@ -34,10 +34,41 @@ class ExpensesApp(QWidget):
         vbox.addWidget(self.date_label)
         vbox.addWidget(self.data_input)
         vbox.addWidget(self.submit_button)
-        vbox.addWidget(self.details_label)
         vbox.addWidget(self.display_button)
 
         self.setLayout(vbox)
+
+        # making widgets aligned to center
+        self.title_label.setAlignment(Qt.AlignCenter)
+
+        # stylling: 
+        # 1) labelling objects
+        self.title_label.setObjectName("title_label")
+        self.category_label.setObjectName("category_label")
+        self.category_input.setObjectName("category_input")
+        self.amount_label.setObjectName("amount_label")
+        self.amount_input.setObjectName("amount_input")
+        self.date_label.setObjectName("date_label")
+        self.data_input.setObjectName("data_input")
+        self.submit_button.setObjectName("submit_button")
+        self.display_button.setObjectName("display_button")
+
+        # 2) css 
+        self.setStyleSheet("""
+            QLabel, QComboBox, QDateEdit, QPushButton, QDoubleSpinBox{
+                  font-family: calibri; 
+                  font-size: 15px;                 
+            }
+            QLabel#title_label{
+                           
+            }
+            QLabel#{
+                           
+            }
+            QLabel#{
+                           
+            }                 
+        """)
 
         # Category - add items, connect signals
         self.category_input.addItems(["Groceries", "Transport", "Communication", "Rent", "Medicine"])
